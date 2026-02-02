5b80fbe62c32e105c7f5c9b0c8158e6540c1430e
grace-mills <gmills.25@presentationhs.org>
2026-02-02 12:30:24 -0800

    creation

---
+++ lab3.py
@@ -0,0 +1,14 @@
+from typing import *
+from dataclasses import dataclass
+import unittest
+import math
+import sys
+sys.setrecursionlimit(10**6)
+class Tests(unittest.TestCase):
+    pass
+# Remember from Lab 1: this if statements checks
+# whether this module (ghg.py) is the module
+# being executed or whether it's just being
+# imported from some other module.
+if (__name__ == '__main__'):
+    unittest.main()
